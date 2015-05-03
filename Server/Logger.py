26b183f2def7e8e1f5394837eef4d4da437fa729
ozelentok <oz.elen@gmail.com>
2015-05-03 21:22:38 +0300

    Fixed crash on non-ascii character on writing to file

--- Server/Logger.py
+++ Server/Logger.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 class Logger(object):
     def __init__(self, log_file_path):
-        self.log_file = open(log_file_path, 'a')
+        self.log_file = open(log_file_path, 'ab')
 
     def log(self, text):
         time_prompt = datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')
-        output_text = u'{} {}\n'.format(time_prompt, text)
+        output_text = (u'{} {}\n'.format(time_prompt, text)).encode('utf-8')
         sys.stdout.write(output_text)
         self.log_file.write(output_text)
         self.log_file.flush()
